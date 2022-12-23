d3da5452901a178df16906d7ca053cb018fb98e8
xcx0902 <99001676+xcx0902@users.noreply.github.com>
2022-12-23 03:23:57 +0000

    Deleted Folder Volume

--- volume/web/template/bshark/footer.php
+++
@@ -1,6 +0,0 @@
-<footer align=center>
-            <p>GPLv2 licensed by <a href='https://github.com/zhblue/hustoj' >HUSTOJ</a> <?php echo date('Y');?><br>
-            THEME bshark by <a href="https://github.com/yemaster">yemaster</a>.
-            
-            </p>
-</footer>
